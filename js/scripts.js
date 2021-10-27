2a50811b3c9c9d164b4ad1a16082c79d64e2baad
Katie Pundt <kzpundt@gmail.com>
2021-10-27 11:46:32 -0700

    outline business logic
    Co-Authored-by: Mike Watkins <michaelhugheswatkins@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,38 @@
 //Utility Logic
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
 
 //Business Logic
+function vowel(word) {
+  const vowels = ["a", "e", "i", "o", "u"]
+  let wordArray = [];
+  wordArray = word.split()
+  if (vowels.includes(wordArray[0])) {
+    let newWord = word + "way";
+    return newWord;
+  }
+  return false;
+}
+
+function consonants(word) {
+
+}
+
+function qu(word) {
+
+}
+
+function pigLatin(text) {
+  //word array
+  //call noInputtedWord
+  //foreach loop over array
+  //call vowel
+  //call qu
+  //call consenonts
+  //close loop
+  //join
+  //return statement
+}
 
 //UI Logic
