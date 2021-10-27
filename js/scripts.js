3f2868cf701f06aefaee9d5add3a7bf33dd087e4
Katie Pundt <kzpundt@gmail.com>
2021-10-27 13:23:22 -0700

    add functions for pig latin rules
    Co-Authored-by: Mike Watkins <michaelhugheswatkins@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,31 +5,35 @@ function noInputtedWord(word, text) {
 
 //Business Logic
 function vowel(word) {
-  const vowels = ["a", "e", "i", "o", "u"]
-  let wordArray = [];
-  wordArray = word.split()
-  if (vowels.includes(wordArray[0])) {
-    let newWord = word + "way";
-    return newWord;
-  }
-  return false;
+  let newWord = "";
+  newWord = word + "way"
+  return newWord;
 }
 
 function consonants(word) {
-
+  let newWord = "";
+  let placeholder = "";
+  let otherPlaceholder = "";
+  placeholder = word.slice(0,1);
+  otherPlaceholder = word.slice(1);
+  newWord = otherPlaceholder + placeholder + "ay";
 }
 
 function qu(word) {
-
+  let newWord = "";
+  newWord = word.slice(2) + "quway";
+  return newWord;
 }
 
 function pigLatin(text) {
   //word array
   //call noInputtedWord
   //foreach loop over array
-  //call vowel
-  //call qu
-  //call consenonts
+  // lowercase word
+  //call vowel if(word[0] === 'a' || "e" || "i" || 'o' || 'u')
+  //call qu if(word[0] === "q" && word[1] === "u")
+  //call consenonts if(word[0] != 'a' || "e" || "i" || 'o' || 'u')
+  //else
   //close loop
   //join
   //return statement
