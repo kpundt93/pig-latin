4ece2070ea746e515fb994e30a7d7a4edfc8e02e
Katie Pundt <kzpundt@gmail.com>
2021-10-27 13:58:35 -0700

    add finished business logic
    Co-Authored-by: Mike Watkins <michaelhugheswatkins@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,48 @@
-//Utility Logic
-function noInputtedWord(word, text) {
-  return ((text.trim().length === 0) || (word.trim().length === 0));
+// Utility Logic
+
+function noInputtedWord(text) {
+  return ((text.trim().length === 0));
 }
 
-//Business Logic
-function vowel(word) {
-  let newWord = "";
-  newWord = word + "way"
-  return newWord;
-}
-
-function consonants(word) {
-  let newWord = "";
-  let placeholder = "";
-  let otherPlaceholder = "";
-  placeholder = word.slice(0,1);
-  otherPlaceholder = word.slice(1);
-  newWord = otherPlaceholder + placeholder + "ay";
-}
-
-function qu(word) {
-  let newWord = "";
-  newWord = word.slice(2) + "quway";
-  return newWord;
-}
+// Business Logic
 
 function pigLatin(text) {
-  //word array
-  //call noInputtedWord
-  //foreach loop over array
-  // lowercase word
-  //call vowel if(word[0] === 'a' || "e" || "i" || 'o' || 'u')
-  //call qu if(word[0] === "q" && word[1] === "u")
-  //call consenonts if(word[0] != 'a' || "e" || "i" || 'o' || 'u')
-  //else
-  //close loop
-  //join
-  //return statement
+  let newSentance = "";
+  
+  if (noInputtedWord(text)) {
+    return false;
+  } else {
+    const wordArray = text.split(" ");
+
+    wordArray.forEach(function(word) {
+      word.toLowerCase()
+      if(word[0] === 'a' || word[0] === "e" || word[0] === "i" || word[0] === 'o' || word[0] === 'u'){
+        let newWord = "";
+        newWord = word + "way"
+        newSentance += newWord + " "; 
+      }
+      else if(word[0] === "q" && word[1] === "u"){
+        let newWord = "";
+        newWord = word.slice(2) + "quway";
+        newSentance += newWord + " ";
+      }
+      else if(word[0] != 'a' && word[0] != "e" && word[0] != "i" && word[0] != 'o' && word[0] != 'u'){
+        let newWord = "";
+        let placeholder = "";
+        let otherPlaceholder = "";
+        placeholder = word.slice(0,1);
+        otherPlaceholder = word.slice(1);
+        newWord = otherPlaceholder + placeholder + "ay";
+        newSentance += newWord + " ";
+      }
+      else{
+        return newSentance
+      }
+      
+    });
+    console.log(newSentance);
+    return newSentance;
+  }
 }
 
-//UI Logic
+// UI Logic
